e04c603ee186e459e49605d3f1ff30586429094c
Jonathan Manera <manerajona@gmail.com>
2020-05-29 09:25:56 -0300

    logging

--- 08.error-handling/00.intro-to-logging/main.go
+++ 08.error-handling/00.intro-to-logging/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-
+	// Open or create a file 'console.log' where text is appended
 	f, _ := os.OpenFile("console.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	// NOTE: ignoring errors
 	defer f.Close()
